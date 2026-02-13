a50e0c356e1287bad261292361c3913cd8d47e0c
D1rtyDr3yJr <dlr060@email.latech.edu>
2026-02-12 19:00:14 -0600

    added some things

--- Email.py
+++ Email.py
@@ -1,9 +1,12 @@
 from postmarker.core import PostmarkClient
+import database
 
 postmark = PostmarkClient(server_token = "d7499ce1-f3d0-4f2b-aa85-1ca332eb1e8c")
 
 senderemail = "dlr060@email.latech.edu"
-useremail = 'jjl034@email.latech.edu'
+DB = database.DB()
+user = DB.GetCurrentUser()
+useremail = user
 
 postmark.emails.send(
 From = senderemail,
